a4c14662cab60c0eebaa0e19c7ba7615741b71e4
Sergey Bolshchikov <sergey@bolshchikov.net>
2017-06-06 12:03:32 +0300

    chore(app): init

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,27 @@
 // @ts-check
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      currentChat: null
+    }
+  }
+
   render() {
     return (
-      <div>
-        This is chat
-        User: {JSON.stringify(this.props.currentUser)}
+      <div className="chat-container">
+        <div className="chat-list-container"></div>
+        {
+          this.state.currentChat ?
+            <div className="chat-conversation-container"></div> :
+            <div className="chat-empty-container">
+              <h1>Welcome to WazzApp</h1>
+            </div>
+        }
       </div>
     );
   }
