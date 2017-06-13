ddea0b93ae11ac6c72c21fd9aaa569bf5fb6dd70
yardenmol <yardenmol12@gmail.com>
2017-06-13 16:34:18 +0300

    implementation of Logout component (#11)

--- src/Logout/index.js
+++ src/Logout/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './Logout.css';
 
-const Logout = ({ onLogout }) => {
-  
+class Logout extends Component {
+  confirmLogout() {
+    const res = window.confirm("Are you sure you want to logout?");
+    if(res) {
+      this.props.onLogout();
+      window.localStorage.removeItem('wazzapp-user');
+    }
+  }
+
+  render() {
+    return (
+      <button onClick={this.confirmLogout.bind(this)}>Logout</button>
+    );
+  }
 }
 
 Logout.propTypes = {
