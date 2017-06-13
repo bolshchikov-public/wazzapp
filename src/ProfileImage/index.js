08d79a24e59f1069c8244c7e9ca7223551923c11
noyagasi <noy.agasi.na@gmail.com>
2017-06-13 16:09:22 +0300

    implement ProfileImage component (#10)

--- src/ProfileImage/index.js
+++ src/ProfileImage/index.js
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 import './ProfileImage.css';
 
 const ProfileImage = ({user}) => {
-  
+  return (
+    <div className="profile-image-container">
+      <img src={user.imageData} alt={user.userName}/>
+    </div>
+  );
 };
 
 ProfileImage.propTypes = {
