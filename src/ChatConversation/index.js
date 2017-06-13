1f0e7c344d7e10a3d4ad68efc36990475f916820
Iddo Alex <IddoAlex@users.noreply.github.com>
2017-06-13 16:50:20 +0300

    Added Chat Conversation Functionality (#9)
    * Added Chat Conversation Functionality
    
    * Fixed HTML, ReceiveProps, getDirection

--- src/ChatConversation/index.js
+++ src/ChatConversation/index.js
@@ -8,6 +8,65 @@ import ConversationInput from '../ConversationInput';
 
 class ChatConversation extends Component {
 
+  constructor(){
+    super();
+    this._clearState();
+  }
+
+  _subscribe(chatId) {
+    if (this.ref !== null) {
+      this.ref.off();
+    }
+    this.ref = firebase.database()
+      .ref(`messages/${chatId}`)
+      .orderByChild('timestamp');
+
+    this.ref.on('child_added', (data) => {
+      // data.val() has the content
+      // data.key has the messageId
+      this.setState((prevState) => {
+        prevState.messages.push(data.val());
+        return {messages: prevState.messages};
+      });
+    });
+  }
+
+  _saveMessage(message) {
+    return firebase.database()
+      .ref(`messages/${this.props.chatId}`)
+      .push(message)
+      .then(data => data.key);
+  }
+
+  _getDirection(item){
+    return item.senderId == this.props.currentUser.phoneNumber ? "to" : "from";
+  }
+
+  _clearState(){
+    this.state = {
+      messages: []
+    };
+}
+
+  componentWillMount() {
+    this._subscribe(this.props.chatId);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this._clearState();
+    this._subscribe(nextProps.chatId);
+  }
+
+  render() {
+    return (
+      <div className="chat-conversation-container">
+        <div className=".chat-conversation-body">
+          {this.state.messages.map((item, index) => <MessageCloud key={index} message={item.body} direction={this._getDirection(item)} />)}
+        </div>
+        <ConversationInput onMessage={this._saveMessage.bind(this)} />
+      </div>
+    )
+  }
 }
 
 ChatConversation.contextTypes = {
