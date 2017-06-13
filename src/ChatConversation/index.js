2431be342db99490e7d176034c9c0c5e25d8a548
Iddo Alex <iddoalex@hotmail.com>
2017-06-13 15:59:40 +0300

    Added Chat Conversation Functionality

--- src/ChatConversation/index.js
+++ src/ChatConversation/index.js
@@ -8,6 +8,54 @@ import ConversationInput from '../ConversationInput';
 
 class ChatConversation extends Component {
 
+  constructor(){
+    super();
+    this.state={
+      messages: []
+    };
+  }
+
+  _subscribe(chatId) {
+    if (this.ref !== null) {
+      this.ref.off();
+    }
+    this.ref = firebase.database()
+      .ref(`messages/${chatId}`)
+      .orderByChild('timestamp');
+
+    this.ref.on('child_added', (data) => {
+      // data.val() has the content
+      // data.key has the messageId
+      this.setState((prevState) => {
+        prevState.messages.push(data.val());
+        return {messages: prevState.messages};
+      });
+    });
+  }
+
+  _saveMessage(message) {
+    return firebase.database()
+      .ref(`messages/${this.props.chatId}`)
+      .push(message)
+      .then(data => data.key);
+  }
+
+  _getDirection(item){
+    return item.sender == this.props.currentUser.key ? "to" : "from";
+  }
+
+  componentWillMount() {
+    this._subscribe(this.props.chatId);
+  }
+
+  render() {
+    return (
+      <div className="chat-conversation-container">
+        {this.state.messages.map((item, index) => <MessageCloud key={index} message={item.body} direction={this._getDirection(item)} />)}
+        <ConversationInput onMessage={this._saveMessage.bind(this)} />
+      </div>
+    )
+  }
 }
 
 ChatConversation.contextTypes = {
