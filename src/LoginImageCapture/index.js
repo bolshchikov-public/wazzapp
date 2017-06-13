efb35c0d19e1422b2051b0b467ae47305d362e07
Shahar Kazaz <shahar.kazaz@gmail.com>
2017-06-13 17:03:31 +0300

    Login capture (#13)
    * test
    
    * start webcam
    
    * * working webcam snapshoot

--- src/LoginImageCapture/index.js
+++ src/LoginImageCapture/index.js
@@ -3,7 +3,49 @@ import classNames from 'classnames';
 import './LoginImageCapture.css';
 
 class LoginImageCapture extends Component {
-  
+    componentDidMount() {
+        let video = document.getElementsByClassName('capture-frame')[0],
+            canvas = document.getElementsByClassName('image-display')[0],
+            captureButton = document.getElementById("capture-button"),
+            rejectButton = document.getElementById("reject-button"),
+            flipper = document.getElementsByClassName("flipper")[0],
+            ctx = canvas.getContext('2d'),
+            localMediaStream = null;
+        function snapshot() {
+            if (localMediaStream) {
+                ctx.drawImage(video, 0, 0, 240, 180);
+                flipper.className += " flip";
+            }
+        }
+
+        captureButton.addEventListener('click', snapshot, false);
+        rejectButton.addEventListener('click', () => {flipper.className = "flipper";}, false);
+        navigator.mediaDevices.getUserMedia({video: true}).then((stream) => {
+            video.src = window.URL.createObjectURL(stream);
+            localMediaStream = stream;
+        });
+    }
+
+    render() {
+        return (
+            <div className="flip-container">
+                <div className="flipper">
+                    <div className="front">
+                        <video className="capture-frame" autoPlay={true}></video>
+                        <div className="capture-actions">
+                            <button id="capture-button" className="button button-small">CAPTURE</button>
+                        </div>
+                    </div>
+                    <div className="back">
+                        <canvas className="image-display"></canvas>
+                        <div className="capture-actions">
+                            <button id="reject-button" className="button button-small button-reject">DISCARD</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default LoginImageCapture
