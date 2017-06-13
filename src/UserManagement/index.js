a0bea8c14ff337595321d7f361c8f9dff7a86400
benmazliach <benmazliach@gmail.com>
2017-06-13 16:07:45 +0300

    user_menagement (#4)
    * bla
    
    * bla
    
    * user-menagement-update

--- src/UserManagement/index.js
+++ src/UserManagement/index.js
@@ -5,7 +5,12 @@ import ProfileImage from '../ProfileImage';
 import Logout from '../Logout';
 
 const UserManagement = ({ onLogout }, { currentUser }) => {
-  
+  return(
+    <div class="user-management-container">
+      <ProfileImage user={currentUser}/>
+      <Logout onLogout={onLogout}/>
+    </div>
+  );
 }
 
 UserManagement.contextTypes = {
