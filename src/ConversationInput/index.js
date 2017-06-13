8b134b9947c4fb7fff5dd479315b733d8c62f71f
EladJy <eladjarby@gmail.com>
2017-06-13 16:18:00 +0300

    Conversation Input and Message Cloud implementation. (#8)
    * :)
    
    * fix body
    
    fix body

--- src/ConversationInput/index.js
+++ src/ConversationInput/index.js
@@ -3,7 +3,21 @@ import PropTypes from 'prop-types';
 import './ConversationInput.css';
 
 class ConversationInput extends Component {
+  
+  _onPress(event) {
+    if(event.key === "Enter") {
+      this.props.onMessage({value: event.target.value});
+      event.target.value = "";
+    }
+  }
 
+  render() {
+    return (
+      <div className="conversation-input-container">
+        <input type="text" placeholder="Write a message" onKeyPress={this._onPress.bind(this)} />
+      </div>
+    )
+  }
 }
 
 ConversationInput.propTypes = {
--- src/MessageCloud/index.js
+++ src/MessageCloud/index.js
@@ -1,11 +1,19 @@
-// @ts-check
+
 import './MessageCloud.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 const MessageCloud = ({ direction, message }) => {
-
+  var time = new Date(message.timestamp);
+  <div className="message-cloud">
+  if(direction == "from") {
+    <span className="message-from">{message.body}</span>
+  } else if(direction == "to") {
+    <span className = "message-to">{message.body}</span>
+  }
+  <span className="message-timestamp">{`${time.getHours()}:${time.getMinutes()}`}</span>
+  </div>
 };
 
 MessageCloud.propTypes = {
