3615a3041125e232d643d2e7921ae3ea6411d4f4
alex@balink <alex@balink.net>
2017-06-13 15:27:05 +0300

    app complete

--- src/App/index.js
+++ src/App/index.js
@@ -8,7 +8,58 @@ import UserManagement from '../UserManagement';
 import FilteredChatList from '../FilteredChatList';
 
 class App extends Component {
+  
+  _creatUserAssosiation(userId1, user2, chatKey) {
+  firebase.database()
+    .ref(`users/${userId1}/chatWith/${user2.phoneNumber}`)
+    .set(chatKey);
+  firebase.database()
+    .ref(`users/${user2.phoneNumber}/chatWith/${userId1}`)
+    .set(chatKey);
+};
 
+_createNewChat(userId1, user2) {
+  firebase.database()
+    .ref('chats')
+    .push({ timestamp: -1 })
+    .then(data => {
+      this.setState({ currentChat: data.key, otherUser : user2 });
+      this._creatUserAssosiation(userId1, user2, data.key);
+    });
+};
+
+handleSelectedUser (user) {
+    //this.setState({otherUser : user }); 
+    firebase.database()
+      .ref(`users/${this.context.currentUser.phoneNumber}/chatWith/${user.phoneNumber}`)
+      .once('value', (snapshot) => {
+        if(typeof snapshot !== 'undefined' && snapshot.val() !== null ) {
+          this.setState({ currentChat: snapshot.val(), otherUser: user });
+          this._creatUserAssosiation(this.context.currentUser.phoneNumber, user, snapshot.val());
+        }
+        else {
+          this._createNewChat(this.context.currentUser.phoneNumber, user);
+        }
+      });
+  }
+  
+  constructor (props) {
+    super(props);
+    this.state = {
+      currentChat : {},
+      otherUser : {}
+    };
+  };
+
+  render () {
+    return (
+      <div>
+        <FilteredChatList onSelect={this.handleSelectedUser} />
+        <UserManagement onLogout={this.props.onLogout}/>
+        <ChatShell opponent={this.state.otherUser.phoneNumber} chatId={this.state.currentChat}/>
+      </div>
+    );
+  };
 }
 
 App.contextTypes = {
